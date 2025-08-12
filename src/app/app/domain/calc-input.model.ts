74cab215d1dd78f7b3488a6f49738759faba837d
Majki <michal@kaminski.com>
2025-08-12 17:27:33 +0200

    feat(domain): define calculation/dto interfaces

--- src/app/app/domain/calc-input.model.ts
+++ src/app/app/domain/calc-input.model.ts
@@ -1,2 +1,8 @@
+import { Period, Tariff } from './tariff.model';
+
 export interface CalcInput {
+  consumptionKwh: number;
+  period?: Period;       // 'year' | 'month' (domyślnie 'year')
+  includeVat?: boolean;  // domyślnie true
+  tariff?: Tariff;       // domyślnie 2023
 }
--- src/app/app/domain/calc-result.model.ts
+++ src/app/app/domain/calc-result.model.ts
@@ -1,2 +1,8 @@
 export interface CalcResult {
+  apMixedCtNet: number;       // ct/kWh netto
+  apMixedEur: number;         // €/kWh brutto/netto (wg includeVat)
+  gpEurForPeriod: number;     // €
+  energyCostEur: number;      // €
+  totalCostEur: number;       // €
+  totalConsumptionKwh: number;
 }
