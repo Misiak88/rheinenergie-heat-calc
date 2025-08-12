94f98dafd08eaff954c49f45eb14550783ab3b29
Majki <michal@kaminski.com>
2025-08-12 17:31:49 +0200

    feat(ui): scaffold CalculatorComponent (standalone)

---
+++ src/app/app/features/calculator/calculator.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  imports: [],
+  template: `
+    <p>
+      calculator works!
+    </p>
+  `,
+  styles: ``
+})
+export class Calculator {
+
+}
